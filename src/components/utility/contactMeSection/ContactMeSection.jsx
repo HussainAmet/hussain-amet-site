53eb13e2b9fb458e7800ae1dc05ba20d63b65ab4
Hussain Amet <hussainamet@gmail.com>
2025-02-11 19:03:57 +0530

    bug fix

--- src/components/utility/contactMeSection/ContactMeSection.jsx
+++ src/components/utility/contactMeSection/ContactMeSection.jsx
@@ -15,7 +15,7 @@ function ContactMeSection() {
             <div className='
                 flex flex-col gap-11
             '>
-                <Text text='Contact Me' className='xl:text-7xl text-4xl font-aptly-medium-italic xl:w-fit lg:w-fit md:w-fit sm:w-fit'/>
+                <Text noRef={true} text='Contact Me' className='xl:text-7xl text-4xl font-aptly-medium-italic xl:w-fit lg:w-fit md:w-fit sm:w-fit'/>
                 <div className='
                     flex justify-between text-xl text-[var(--accent)]
                     md:flex-col md:gap-11
--- src/components/utility/text/Text.jsx
+++ src/components/utility/text/Text.jsx
@@ -10,28 +10,30 @@ function Text({ text, className = '', ...props }) {
   const textRef = useRef(null);
 
   useEffect(() => {
-    const textArr = text.split("");
-    const clutter = textArr.map(letter => `<span class="block text-span">${letter}</span>`).join('');
-    textRef.current.innerHTML = clutter;
-
-    gsap.from(textRef.current.querySelectorAll('.text-span'), {
-      y: 90,
-      opacity: 0,
-      duration: 0.2,
-      delay: 0.5,
-      stagger: 0.1,
-      ease: "bounce.out",
-      scrollTrigger: {
-        trigger: textRef.current,
-        start: "top bottom",
-        toggleActions: "play none none reverse",
-      }
-    });
-  }, [text]);
+    if (!props.noRef) {
+      const textArr = text.split("");
+      const clutter = textArr.map(letter => `<span class="block text-span">${letter}</span>`).join('');
+      textRef.current.innerHTML = clutter;
+  
+      gsap.from(textRef.current.querySelectorAll('.text-span'), {
+        y: 90,
+        opacity: 0,
+        duration: 0.2,
+        delay: 0.5,
+        stagger: 0.1,
+        ease: "bounce.out",
+        scrollTrigger: {
+          trigger: textRef.current,
+          start: "top bottom",
+          toggleActions: "play none none reverse",
+        }
+      });
+    }
+  }, [text, props.noRef]);
 
   return (
     <div
-      ref={textRef}
+      ref={props.noRef ? null : textRef}
       className={`
         ${className} text-span
         font-aptly-regular-italic flex justify-center overflow-hidden whitespace-pre
@@ -42,7 +44,7 @@ function Text({ text, className = '', ...props }) {
         w-[224px] text-[44px] h-[52px]
       `}
       {...props}
-    ></div>
+    >{props.noRef ? text : ""}</div>
   );
 }
 
