ebdc04a2c5d4697fc77698bdc2b5a5d8273cbd6e
Hussain Amet <hussainamet@gmail.com>
2025-07-10 23:27:33 +0530

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,19 @@
+import Head from "next/head";
 import { PageContainer } from "@/components/utility/container/Container";
 import Home from "@/page/Home";
 
 export default function HomePage() {
   return (
-    <PageContainer>
-      <Home />
-    </PageContainer>
+    <>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="dOTMamZFyXCHILsW_4b_liGAGGEVNWcTFDbKQLxxI6M"
+        />
+      </Head>
+      <PageContainer>
+        <Home />
+      </PageContainer>
+    </>
   );
 }
