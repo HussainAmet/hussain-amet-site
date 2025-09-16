53a9dd2dafab114464b5e3556dcedaab7029edc6
Hussain Amet <hussainamet@gmail.com>
2025-09-16 22:16:41 +0530

    Update route.js

--- src/app/(sitemap)/sitemap.xml/route.js
+++ src/app/(sitemap)/sitemap.xml/route.js
@@ -2,11 +2,6 @@ export async function GET() {
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
-      <loc>https://hussain-amet-site.vercel.app/</loc>
-      <lastmod>${new Date().toISOString().split("T")[0]}</lastmod>
-      <priority>1.0</priority>
-    </url>
-    <url>
       <loc>https://hussain-amet-site.vercel.app/about-me</loc>
       <lastmod>${new Date().toISOString().split("T")[0]}</lastmod>
       <priority>0.9</priority>
